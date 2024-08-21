2d3dc1baaec068ad0b46f32804fd4ad1113602d9
Resha Munikar <resa.munikar@gmail.com>
2024-08-21 21:09:18 +0545

    Sum added

---
+++ src/Greet.java
@@ -0,0 +1,10 @@
+//Program to ask user to enter his/her name and greet them
+import  java.util.*;
+public class Greet {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + ", have a good day.");
+    }
+}
---
+++ src/Sum.java
@@ -0,0 +1,15 @@
+//Program to add three numbers
+import java.util.*;
+public class Sum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter third number : ");
+        int num3 = sc.nextInt();
+        int sum = num1 + num2 + num3;
+        System.out.println(num1 + " + " + num2 + " + " + num3 + " = " + sum);
+    }
+}
