57a1593385e672076e414a198c91f7b6cc8e3c29
Resha Munikar <resa.munikar@gmail.com>
2024-09-02 21:54:45 +0545

    Thread demo added

--- src/Lab3/ThreadDemo.java
+++ src/Lab3/ThreadDemo.java
@@ -1,4 +1,39 @@
 package Lab3;
-
+class OddThread implements Runnable{
+    public void run(){
+        for(int i=0; i<10; i++) {
+            try {
+                Thread.sleep(2000);
+            }catch(InterruptedException e){
+                System.out.println("Error in "+ Thread.currentThread().getName());
+            }
+            if(i%2==1){
+                System.out.println(i);
+            }
+        }
+    }
+}
+class EvenThread extends Thread{
+    public void run(){
+        for(int i=0; i<5; i++) {
+            try {
+                Thread.sleep(3000);
+            }catch(InterruptedException e){
+                System.out.println("Error in "+ Thread.currentThread().getName());
+            }
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
 public class ThreadDemo {
+    public static void main(String[] args) {
+        OddThread odd = new OddThread();
+        EvenThread even = new EvenThread();
+        Thread thread1 = new Thread(odd);
+        thread1.start();
+        Thread thread2 = new Thread(even);
+        thread2.start();
+    }
 }
