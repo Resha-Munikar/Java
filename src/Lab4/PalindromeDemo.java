88a9eb6e2b9fdfe914bfe094ad3752db246d783e
Resha Munikar <resa.munikar@gmail.com>
2024-09-05 20:56:26 +0545

    palindrome added

--- src/Lab4/PalindromeDemo.java
+++ src/Lab4/PalindromeDemo.java
@@ -3,11 +3,18 @@ import java.util.*;
 public class PalindromeDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String rev ="";
         System.out.println("Enter a word : ");
         String word = sc.nextLine();
         int len = word.length();
-        for(int i=len; i>=0; i--){
-            
+        for(int i=len-1; i>=0; i--){
+            rev += word.charAt(i);
+        }
+        if(word.equals(rev)){
+            System.out.println(word+" is palindrome.");
+        }
+        else{
+            System.out.println(word+" is not palindrome.");
         }
     }
 }
