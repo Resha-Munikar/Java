86d285dce74c2ee5eab392248ed32b84f37e2292
Resha Munikar <resa.munikar@gmail.com>
2024-09-10 08:53:32 +0545

    Account form added

--- src/GUI/AccountForm.java
+++ src/GUI/AccountForm.java
@@ -1,8 +1,6 @@
 package GUI;
-import javax.imageio.stream.ImageInputStream;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 public class AccountForm extends JFrame {
     AccountForm() {
         setLayout(null);
@@ -30,8 +28,56 @@ public class AccountForm extends JFrame {
         JTextField tf3 = new JTextField();
         tf3.setBounds(120,110,300,30);
         add(tf3);
+        JLabel lb4 = new JLabel("Country/Region *");
+        lb4.setBounds(5,130,100,60);
+        add(lb4);
+        String[] countries = {"Nepal","India","China","Bhutan","Maldives","Australia","USA"};
+        JComboBox<String> combo = new JComboBox<>(countries);
+        combo.setBounds(120,150,300,30);
+        add(combo);
+        JLabel lb5 = new JLabel("Street Address *");
+        lb5.setBounds(5,170,100,60);
+        add(lb5);
+        JTextField tf4 = new JTextField();
+        tf4.setBounds(120,190,300,30);
+        add(tf4);
+        JLabel lb6 = new JLabel("Street Address 2");
+        lb6.setBounds(5,210,100,60);
+        add(lb6);
+        JTextField tf5 = new JTextField();
+        tf5.setBounds(120,230,300,30);
+        add(tf5);
+        JLabel lb7 = new JLabel("City *");
+        lb7.setBounds(5,250,100,60);
+        add(lb7);
+        JTextField tf6 = new JTextField();
+        tf6.setBounds(120,270,300,30);
+        add(tf6);
+        JLabel lb8 = new JLabel("State/Province *");
+        lb8.setBounds(5,290,100,60);
+        add(lb8);
+        String[] province = {"Bagmati","Gandaki","Karnali","Koshi","Sudurpaschim"};
+        JComboBox<String> provincebox = new JComboBox<>(province);
+        provincebox.setBounds(120,310,220,30);
+        add(provincebox);
+        JLabel lb9 = new JLabel("Zip/Postal Code *");
+        lb9.setBounds(5,330,100,60);
+        add(lb9);
+        JTextField tf7 = new JTextField();
+        tf7.setBounds(120,350,100,30);
+        add(tf7);
+        JLabel lb10 = new JLabel("Only used for questions related to your order.");
+        lb10.setBounds(120,390,270,15);
+        add(lb10);
+        JLabel lb11 = new JLabel("Phone Number *");
+        lb11.setBounds(5,400,100,60);
+        add(lb11);
+        JTextField tf8 = new JTextField();
+        tf8.setBounds(120,420,300,30);
+        add(tf8);
         setSize(500,500);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
