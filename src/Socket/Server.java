6d7c3f28c2fe53607e7995254aabe9c592e66ff0
Resha Munikar <resa.munikar@gmail.com>
2024-11-07 19:59:55 +0545

    Client added

--- src/Socket/Server.java
+++ src/Socket/Server.java
@@ -5,9 +5,11 @@ public class Server {
     public static void main(String[] args) {
         try{
             ServerSocket ss = new ServerSocket(6666);
+            System.out.println("Server started.");
             Socket s = ss.accept();
+            System.out.println("Connection established");
             DataInputStream dis = new DataInputStream(s.getInputStream());
-            String str = (String)dis.readUTF();
+            String str = dis.readUTF();
             System.out.println("Message = "+ str);
             ss.close();
         }catch(Exception e){
