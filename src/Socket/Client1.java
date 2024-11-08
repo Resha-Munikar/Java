6a3d6ec5fff163423b99709effce347086f32cda
Resha Munikar <resa.munikar@gmail.com>
2024-11-08 21:10:48 +0545

    updated

--- src/Socket/Client1.java
+++ src/Socket/Client1.java
@@ -1,19 +1,20 @@
 package Socket;
 import java.io.*;
+
 import java.net.*;
-import java.util.*;
+        import java.util.*;
 public class Client1 {
     public static void main(String[] args) {
         try{
-            Socket s = new Socket("localhost",1234);
+            Socket s = new Socket("localhost",4444);
             Scanner sc = new Scanner(System.in);
-            PrintWriter pw = new PrintWriter(s.getOutputStream());
+            PrintWriter pw = new PrintWriter(s.getOutputStream(),true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            pw.println("Enter your name : ");
+            System.out.println("Enter your name : ");
             String name = sc.nextLine();
-            pw.println("Name : " + name);
+            pw.println(name);
             String convertedName = reader.readLine();
-            pw.println("Converted name : "+ convertedName);
+            System.out.println("Converted name : "+ convertedName);
             s.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
--- src/Socket/Server1.java
+++ src/Socket/Server1.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class Server1 {
     public static void main(String[] args) {
         try{
-            ServerSocket ss = new ServerSocket(1234);
+            ServerSocket ss = new ServerSocket(4444);
             Socket s = ss.accept();
             PrintWriter pw = new PrintWriter(s.getOutputStream(),true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -12,7 +12,7 @@ public class Server1 {
             pw.println(name.toUpperCase());
             ss.close();
         }catch(Exception e){
-            System.out.println(e);
+            System.out.println("Error");
         }
     }
 }
