89d89f9e351fdc34473fdd8b1ebce2811e4a1983
Resha Munikar <resa.munikar@gmail.com>
2024-11-09 20:33:12 +0545

    Client 2 added

---
+++ src/Socket/Client2.java
@@ -0,0 +1,28 @@
+package Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class Client2 {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+            Socket s = new Socket("localhost",1234);
+            PrintWriter pw = new PrintWriter(s.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            System.out.println("Enter first number : ");
+            int num1 = sc.nextInt();
+            System.out.println("Enter second number : ");
+            int num2 = sc.nextInt();
+            pw.println(num1);
+            pw.println(num2);
+            int sum = Integer.parseInt(reader.readLine());
+            int diff = Integer.parseInt(reader.readLine());
+            int mul = Integer.parseInt(reader.readLine());
+            System.out.println("Sum : "+ sum);
+            System.out.println("Difference : "+ diff);
+            System.out.println("Product : "+ mul);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
